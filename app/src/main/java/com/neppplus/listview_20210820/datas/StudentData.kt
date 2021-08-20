971ae8b602b6399691fc84a85d48926dc729d154
조경진 <cho881020@gmail.com>
2021-08-20 15:44:01 +0900

    학생 클래스의 기능 (fun) 추가  - 년도를 넣으면, 나이를 구해주는 기능

--- app/src/main/java/com/neppplus/listview_20210820/datas/StudentData.kt
+++ app/src/main/java/com/neppplus/listview_20210820/datas/StudentData.kt
@@ -5,5 +5,14 @@ class StudentData(
     val birthYear : Int,
     val address : String)  {
 
+//    학생의 기능으로 => 나이를 계산해서 알려주는 기능? 함수로 만들자.
+
+    fun getKoreanAgeByYear( year : Int ) : Int {
+
+        val age = year - this.birthYear + 1
+
+        return age
+
+    }
 
 }
