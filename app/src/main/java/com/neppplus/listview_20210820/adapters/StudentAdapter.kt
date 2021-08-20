a1893b62adad9a9dbcc0d5bb759b3cad21d4d0f7
조경진 <cho881020@gmail.com>
2021-08-20 12:01:02 +0900

    실제 데이터를 리스트뷰에 반영

--- app/src/main/java/com/neppplus/listview_20210820/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20210820/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppplus.listview_20210820.R
 import com.neppplus.listview_20210820.datas.StudentData
 import java.util.zip.Inflater
@@ -26,6 +27,21 @@ class StudentAdapter(
 
         val row = tempRow!!
 
+
+//        row가 결과로 (화면에) 나가기 전에 => 추가 가공을 거치고 나가게 하자.
+
+//        mList의 학생중 위치에 맞는 데이터를 뽑아서 => row 내부의 텍스트뷰에 문구로 반영하자.
+
+        val data = mList[position]
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+        val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
+
+        nameTxt.text = data.name
+        addressTxt.text = data.address
+
+        ageTxt.text = "(${2021 - data.birthYear + 1} 세)"
 
         return row
     }
