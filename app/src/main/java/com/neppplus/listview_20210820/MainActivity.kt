2ab256f51449e76d62e6e82646146a9c7f45016d
조경진 <cho881020@gmail.com>
2021-08-20 12:11:59 +0900

    리스트뷰의 이벤트처리

--- app/src/main/java/com/neppplus/listview_20210820/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20210820/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.listview_20210820
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.neppplus.listview_20210820.adapters.StudentAdapter
 import com.neppplus.listview_20210820.datas.StudentData
 import kotlinx.android.synthetic.main.activity_main.*
@@ -33,6 +34,13 @@ class MainActivity : AppCompatActivity() {
 
         studentListView.adapter = mAdapter
 
+        studentListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val clickedStudent = mStudentList[position]
+
+            Toast.makeText(this, clickedStudent.name, Toast.LENGTH_SHORT).show()
+
+        }
 
     }
 }
